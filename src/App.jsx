7dc4678376c632821d6a9c647909a166cee59b5f
lloydgapton <lloydgapton123@gmail.com>
2024-12-26 16:43:24 -0800

    my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Main from "./components/Main";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header"
+import Entry from "./components/Entry"
+import data from "./data"
 
-export default function App(){
+export default function App (){
+  const entryElement = data.map((entry)=>{
+    return(
+      <Entry
+      key={entry.id}
+      {...entry} />
+    )
+  })
   return(
     <>
-    <Navbar />
-    <Main />
+    <Header />
+    <main className="container">
+      {entryElement}
+    </main>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import globeimage from "/assets/globe.png"
+export default function(){
+    return(
+        <header>
+            <img src={globeimage} alt="global icon"></img>
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
